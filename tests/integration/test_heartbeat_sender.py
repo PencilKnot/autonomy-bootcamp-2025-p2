c6c0057fd1f9240c9fa197fa2c575cc67a28b397
Wing Chang <wingchang.ca@gmail.com>
2026-01-14 23:00:03 -0500

    Added heartbeat receiver and sender

--- tests/integration/test_heartbeat_sender.py
+++ tests/integration/test_heartbeat_sender.py
@@ -38,19 +38,19 @@ def start_drone() -> None:
     """
     Start the mocked drone.
     """
-    subprocess.run(["python", "-m", MOCK_DRONE_MODULE], shell=True, check=False)
+    subprocess.run(["python", "-m", MOCK_DRONE_MODULE], check=False)
 
 
 # =================================================================================================
 #                            ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
 # =================================================================================================
 def stop(
-    args,  # Add any necessary arguments
+    controller: worker_controller.WorkerController
 ) -> None:
     """
     Stop the workers.
     """
-    pass  # Add logic to stop your worker
+    controller.request_exit()
 
 
 # =================================================================================================
@@ -93,13 +93,16 @@ def main() -> int:
     # =============================================================================================
     # Mock starting a worker, since cannot actually start a new process
     # Create a worker controller for your worker
+    controller = worker_controller.WorkerController()
 
     # Just set a timer to stop the worker after a while, since the worker infinite loops
-    threading.Timer(HEARTBEAT_PERIOD * NUM_TRIALS, stop, (args,)).start()
+    threading.Timer(
+        HEARTBEAT_PERIOD * NUM_TRIALS,
+        stop,
+        (controller,),
+    ).start()
 
-    heartbeat_sender_worker.heartbeat_sender_worker(
-        # Place your own arguments here
-    )
+    heartbeat_sender_worker.heartbeat_sender_worker(connection, controller)
     # =============================================================================================
     #                          ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
     # =============================================================================================
