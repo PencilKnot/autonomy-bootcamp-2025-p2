c205a953ef93cc0f0cb8d111dcd53e1702aa887f
Wing Chang <wingchang.ca@gmail.com>
2026-03-01 23:01:26 -0500

    Fixed formatting issues

--- tests/integration/test_heartbeat_receiver.py
+++ tests/integration/test_heartbeat_receiver.py
@@ -61,7 +61,9 @@ def stop(
     main_logger.info("Stopped workers", True)
 
 
-def read_queue(args: object, main_logger: logger.Logger, queue: mp.Queue) -> None:  # Add any necessary arguments
+def read_queue(
+    args: object, main_logger: logger.Logger, queue: mp.Queue
+) -> None:  # Add any necessary arguments
     """
     Read and print the output queue.
     """
